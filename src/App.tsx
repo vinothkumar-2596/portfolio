56e7b2778a2138b68004bed11ebae290ceabf757
SMVEC2025 <webdeveloper@smvec.ac.in>
2025-08-29 15:35:06 +0530

    last

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import Header from '@components/sections/header'
-import About from '@components/sections/about'
 import Hero from '@components/sections/hero'
-import Partners from '@components/sections/partners'
-import RecentProjects from '@components/sections/recentProjects'
-import WorkingProcess from '@components/sections/workingProcess'
-import Services from '@components/sections/services'
+
 import Projects from '@components/sections/projects'
-import Testimonials from '@components/sections/testimonials'
-import Features from '@components/sections/features'
-import Pricing from '@components/sections/pricing'
-import Faqs from '@components/sections/faqs'
+
 import Booking from '@components/sections/booking'
 import Footer from '@components/sections/footer'
 import ScrollToTop from '@components/sections/scrollToTop'
