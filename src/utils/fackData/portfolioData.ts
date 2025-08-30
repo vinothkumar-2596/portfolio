d9e6977eaffda2cc5fa079112d6f494bbc1bc17e
SMVEC2025 <webdeveloper@smvec.ac.in>
2025-08-30 09:57:27 +0530

    changes

--- src/utils/fackData/portfolioData.ts
+++ src/utils/fackData/portfolioData.ts
@@ -3,14 +3,14 @@ export const portfolioData = [
         id: 1,
         title: "Library Management",
         img: "/images/project1.png",
-        category: "Branding",
+        category: "UI/UX",
         link: 'https://www.figma.com/design/WWaewEE2Vugm80dYX1oaDy/LMS---Forms-UI?node-id=0-1&p=f'
     },
     {
         id: 2,
         title: "Cloud Clinic",
         img: "/images/project2.png",
-        category: "Design",
+        category: "UI/UX",
         link: 'https://www.figma.com/design/hoMQQHBe6Lxcu5VJodBOHT/Cloud-Clinic-UI-UX?node-id=0-1&p=f'
 
     },
@@ -18,7 +18,7 @@ export const portfolioData = [
         id: 3,
         title: "My Medical Shop",
         img: "/images/project3.png",
-        category: "Graphics",
+        category: "UI/UX",
         link: 'https://www.figma.com/design/a3GB299ZpsOpug5LYvTcfW/MMS-UI---Handoff-Ver.-file?node-id=0-1&p=f'
 
     },
